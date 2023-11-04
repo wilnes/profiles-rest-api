ce47e98a54b4936de4124a3b3d77de12476f2b57
Wilfried <wilfrieddjahou@gmail.com>
2023-11-04 20:29:38 +0100

    Added hello api view

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test API view"""
+
+    def get(self, request, format=None):
+        """Returns a list of APIView features"""
+
+        an_apiview = [
+            'Uses http method as function get,post,patch,put,delete',
+            'Is smilar ta a traditional Django view',
+            'Give you the most control over your application'
+        ]
+
+        return Response({'message': "Hello", 'an_apiview': an_apiview})
